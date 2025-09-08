4977d93be0ad60cbd0be8b7bdcc09c85ebf16276
Komal Sharma <145161424+komal-sharma19@users.noreply.github.com>
2025-09-08 12:39:07 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -80,7 +80,7 @@ def create_wordcloud(selected_user, df):
     
     wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white')
     temp['message']=temp['message'].apply(remove_stop_words)
-    df_wc=wc.generate(temp['message'].str.cat(sep=" "))
+    df_wc=wc.generate(temp['message'].astype(str).cat(sep=" "))
     return df_wc
 
 
@@ -181,4 +181,5 @@ def activity_heatmap(selected_user,df):
     
 
     
+
     
